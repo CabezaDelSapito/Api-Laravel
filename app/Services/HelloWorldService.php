f5ca9d277c02ef449e57baabf292490cad7e6169
José Olímpio <olimpiokbz@gmail.com>
2024-06-28 11:19:08 -0300

    arquitetura hexagonal

--- app/Services/HelloWorldService.php
+++ app/Services/HelloWorldService.php
@@ -2,11 +2,17 @@
 
 namespace App\Services;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class HelloWorldService
 {
-    public function get(): string
+    public function get(): Collection
     {
         return User::get();
+        //$users = User::all();
+        // Constrói uma string com os nomes dos usuários
+        // $userNames = $users->pluck('name')->implode(', ');
+        // return "Lista de usuarios: " . $userNames;
     }
 }
+
